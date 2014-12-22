4faad991fe6637f78b11009476750eb052f99f68
unknown <snamfukwe@g.harvard.edu>
2014-12-22 18:28:45 -0500

    troubleshooting

--- app/views/search_results.blade.php
+++ app/views/search_results.blade.php
@@ -12,7 +12,7 @@
 		<div class='flash-message'>{{ Session::get('flash_message') }}</div>
 	@endif
 
-	@if ($goal->goal_completed === 1)
+
 		
 		<h2>{{ $goal->name}}</h2>
 		<p>{{ Form::label('name',$goal->name) }} You Completed this goal on: </p>
@@ -24,22 +24,6 @@
     <p>Date Created: 
     	{{ Form::label('created_at',$goal->created_at) }}</p>
 
-	
-	</p>
-			<a href='/goal/edit/{{ $goal->id }}'>Edit</a> <br><br> 
-						<a href='/'>Return To Homepage</a>
-	@endif
-
-	@if ($goal->goal_completed === 0)
-
-		<h2>{{ $goal->name }}</h2>
-		<p>{{ Form::label('name',$goal->name) }}: Has Not Yet Been Completed{{ Form::label('decription', $goal->description) }}</p>
-	   
-	   <p>Date Created: 
-    	{{ Form::label('created_at',$goal->created_at) }}</p>
-			<a href='/goal/edit/{{ $goal->id }}'>Edit</a> <br><br> 			
-			<a href='/'>Return To Homepage</a>
-	@endif
 
 </body>
 </html>
