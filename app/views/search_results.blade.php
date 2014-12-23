83a5100b057513ec5f72b88d464ca139c5616a75
unknown <snamfukwe@g.harvard.edu>
2014-12-22 23:31:03 -0500

    more troubleshooting

--- app/views/search_results.blade.php
+++ app/views/search_results.blade.php
@@ -13,27 +13,37 @@
 	@endif
 
 
-		
+	@if ($goal->goal_completed == '1')	
+	<div class='grey'>
 		<h2>{{ $goal->name}}</h2>
-		<p>{{ Form::label('name',$goal->name) }} You Completed this goal on: </p>
-		<p> 
+		<p>{{ Form::label('name',$goal->name) }} </p>
+		
+		<p>Goal Description{{ Form::label('decription', $goal->description) }}</p>
+
+    <p>Date Created: 
+    	{{ Form::label('created_at',$goal->created_at) }}</p>
+	<p>Date Completed: 
 		{{ Form::label('updated_at',$goal->updated_at) }}</p>
+	</div>
+			<a href='/goal/edit/{{ $goal->id }}'>Edit</a> <br><br> 
+						<a href='/'>Return To Homepage</a>
+
+
+@elseif ($goal->goal_completed == '0')
+<div class='orange'>
+    <h2>{{ $goal->name}}</h2>
+		<p>{{ Form::label('name',$goal->name) }} </p>
 
 		<p>Goal Description{{ Form::label('decription', $goal->description) }}</p>
 
     <p>Date Created: 
     	{{ Form::label('created_at',$goal->created_at) }}</p>
-	</p>
+	</p> </div>
 			<a href='/goal/edit/{{ $goal->id }}'>Edit</a> <br><br> 
 						<a href='/'>Return To Homepage</a>
-
-
-@if ($goal->goal_completed == '1')
-    <p>Something is red, white, and brown!</p>
-@elseif ($goal->goal_completed == '0')
-    <p>Something is black and white!</p>
 @else
-    <p>You currently have no goals .</p>
+    <p>Sorry, we did not find any goals for you but you can always create new goals. Good Luck! Click below to start</p>
+    <a href='/goal/add'>Create a new goal</a>
 @endif
 
 </body>
