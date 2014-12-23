1347eec9192e1d2dd5686e43e40466ce79c950e4
unknown <snamfukwe@g.harvard.edu>
2014-12-23 04:21:09 -0500

    never been more stressed out in my entire life...but looks like all bugs are gone. FINAL COMMIT

--- app/views/goal_edit.blade.php
+++ app/views/goal_edit.blade.php
@@ -34,10 +34,7 @@
 			{{ Form::text('created_at',$goal->created_at) }}
 
 		@endif
-			Adding a Note is Optional:
-			{{ Form::label('note', 'Note: ') }}
-    		{{ Form::text('note', 'add note here') }}
-
+			
 			{{ Form::hidden('id', 'ID') }}
 			{{ Form::hidden('id', $goal->id) }}	
 
